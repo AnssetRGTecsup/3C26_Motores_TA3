fcdabed80e4788e137acaeefd413fc0d70c550bd
Zero-Ackerman-z <144136238+Zero-Ackerman-z@users.noreply.github.com>
2024-10-29 12:00:27 -0500

    Avance: Niveles

--- Assets/Scripts/FinishController.cs
+++ Assets/Scripts/FinishController.cs
@@ -1,18 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishController : MonoBehaviour
 {
+    [SerializeField] private string newScene;
+
     private void OnCollisionEnter(Collision other) {
         if(other.gameObject.CompareTag("Player")){
-            Debug.Log("GANO!");
-        }
-    }
-
-    private void OnTriggerEnter(Collider other) {
-        if(other.CompareTag("Player")){
-            Debug.Log("GANO!");
+            SceneManager.LoadScene(newScene);
         }
     }
 }
