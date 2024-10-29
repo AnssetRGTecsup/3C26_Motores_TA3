42450bb89cb057eb5abdf7992ecd45abd270d998
victorcollazosisnull <victor.collazos@tecsup.edu.pe>
2024-10-29 11:49:41 -0500

    commit 2

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -47,6 +47,10 @@ public class BallController : MonoBehaviour
         {
             materialController.ChangeEmissionColor(MaterialChange.OnOnlyVertical);
         }
+        else if (modifier == Vector2.right)
+        {
+            materialController.ChangeEmissionColor(MaterialChange.OnOnlyHorizontal);
+        }
         myRGBD.velocity = Vector3.Scale(myRGBD.velocity, modifier);
     }
     private void OnEnable()
