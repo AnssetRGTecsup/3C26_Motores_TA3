028921d6512acf337f20b9847375863b43b2db50
YitssTG <ismaelhuaman300@gmail.com>
2025-05-22 12:56:16 -0500

    Nivel Implemented

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -54,7 +54,6 @@ public class BallController : MonoBehaviour
         {
             myRGBD.linearVelocity = new Vector3(myRGBD.linearVelocity.x, 0f, 0f);
             materialController.ChangeEmissionColor(MaterialChange.OnOnlyVertical);
-
         }
         if (other.CompareTag("Gravity"))
         {
