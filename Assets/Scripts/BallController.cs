912fa369500f6f567ac3c0d0b09ccf0f95b462dd
Alumno <john.ayudante@tecsup.edu.pe>
2025-05-22 12:37:13 -0500

    Mechanic collors complete

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -47,14 +47,19 @@ public class BallController : MonoBehaviour
         if (other.CompareTag("Horizontal"))
         {
             myRGBD.linearVelocity = new Vector3(0f, myRGBD.linearVelocity.y, 0f);
+            materialController.ChangeEmissionColor(MaterialChange.OnOnlyHorizontal);
+
         }
         if (other.CompareTag("Vertical"))
         {
             myRGBD.linearVelocity = new Vector3(myRGBD.linearVelocity.x, 0f, 0f);
+            materialController.ChangeEmissionColor(MaterialChange.OnOnlyVertical);
+
         }
         if (other.CompareTag("Gravedad"))
         {
             OngravityTrigger?.Invoke();
+            materialController.ChangeEmissionColor(MaterialChange.OnLooseGravity);
         }
     }
 }
