75de870519c66b71bc6f74ad0c37eb1027fea1f2
Firestar04O <ricardo.espino.b@tecsup.edu.pe>
2025-05-22 12:00:50 -0500

    vertical implementated

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -47,6 +47,10 @@ public class BallController : MonoBehaviour
         {
             myRGBD.linearVelocity = new Vector3(0, myRGBD.linearVelocity.y, myRGBD.linearVelocity.z);
         }
+        if (other.CompareTag("VerticalPowerUp"))
+        {
+            myRGBD.linearVelocity = new Vector3(myRGBD.linearVelocity.x, 0, myRGBD.linearVelocity.z);
+        }
     }
 
 }
