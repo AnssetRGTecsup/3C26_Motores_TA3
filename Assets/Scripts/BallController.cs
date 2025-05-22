ca0e27f0d1d634e2f052929cb61ced473a730745
alondra19ra <alondra.rojas@tecsup.edu.pe>
2025-05-22 12:38:05 -0500

    chance color complete

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -49,14 +49,17 @@ public class BallController : MonoBehaviour
         if (other.CompareTag("Horizontal"))
         {
             myRGBD.linearVelocity = new Vector3(0f, myRGBD.linearVelocity.y, 0f);
+            materialController.ChangeEmissionColor(MaterialChange.OnOnlyVertical);
         }
         if (other.CompareTag("Vertical"))
         {
             myRGBD.linearVelocity = new Vector3(0f, myRGBD.linearVelocity.x, 0f);
+            materialController.ChangeEmissionColor(MaterialChange.OnOnlyHorizontal);
         }
         if (other.CompareTag("Gravedad"))
         {
             OnGravityTrigger?.Invoke();
+            materialController.ChangeEmissionColor(MaterialChange.OnLooseGravity);
 
         }
     }
